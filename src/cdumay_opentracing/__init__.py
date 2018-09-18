ae4988f000c2b84d9b1a330ee62b745b81b338c0
Cedric Dumay <cedric.dumay@gmail.com>
2018-09-18 18:34:47 +0200

    devel: rewrote lib

--- src/cdumay_opentracing/__init__.py
+++ src/cdumay_opentracing/__init__.py
@@ -7,106 +7,135 @@
 
 """
 import logging
-
 import opentracing
+from jaeger_client import Span
 
 logger = logging.getLogger(__name__)
 
 
-class OpenTracingDriver(object):
+class SpanProxy(object):
     FORMAT = opentracing.Format.TEXT_MAP
     TAGS = []
 
-    @classmethod
-    def extract(cls, data):
-        """ Extract span context from a `carrier` object
+    def __init__(self, span, obj):
+        self.span = span
+        self.obj = obj
 
-        :param Any data: the `carrier` object.
-        :return: a SpanContext instance extracted from `carrier` or None if no
+    @classmethod
+    def name(cls, obj):
+        """ Extract span name from the given object
+
+        :param Any obj: Object to use as context
+        :return: Span name
+        :rtype: str
+        """
+        return str(obj)
+
+    @classmethod
+    def extract(cls, obj):
+        """ Extract span context from the given object
+
+        :param Any obj: Object to use as context
+        :return: a SpanContext instance extracted from the inner span object or None if no
             such span context could be found.
         """
-        return opentracing.tracer.extract(cls.FORMAT, data)
+        return opentracing.tracer.extract(cls.FORMAT, obj)
 
     @classmethod
-    def inject(cls, span, data):
-        """ Injects the span context into a `carrier` object.
+    def extract_tags(cls, obj):
+        """ Extract tags from the given object
 
-        :param opentracing.span.SpanContext span: the SpanContext instance to inject
-        :param Any data: the `carrier` object.
-        """
-
-        opentracing.tracer.inject(span, cls.FORMAT, data)
-
-    @classmethod
-    def tags(cls, data):
-        """ Extract tags from `carrier` object.
-
-        :param Any data: the `carrier` object.
+        :param Any obj: Object to use as context
         :return: Tags to add on span
         :rtype: dict
         """
-        return vars(data)
+        return dict(
+            [(attr, getattr(obj, attr, None)) for attr in cls.TAGS]
+        )
 
     @classmethod
-    def log_kv(cls, span, data, event, **kwargs):
-        """ Adds a log record to the Span.
+    def inject(cls, span, obj):
+        """ Injects the span context into a `carrier` object.
 
-        :param opentracing.span.Span span: the Span instance to use.
-        :param Any data: the `carrier` object.
-        :param str event: Span event name.
-        :param dict kwargs: A dict of string keys and values of any type to log
-        :return: Returns the Span itself, for call chaining.
-        :rtype: opentracing.span.Span
+        :param opentracing.span.SpanContext span: the SpanContext instance
+        :param Any obj: Object to use as context
+        """
+        opentracing.tracer.inject(span, cls.FORMAT, obj)
+
+    @classmethod
+    def prerun(cls, span, obj, **kwargs):
+        """ Trigger to execute just after span creation
+
+        :param opentracing.span.SpanContext span: the SpanContext instance
+        :param Any obj: Object to use as context
+        :param dict kwargs: additional data
+        """
+
+    @classmethod
+    def postrun(cls, span, obj, **kwargs):
+        """ Trigger to execute just before closing the span
+
+        :param opentracing.span.Span  span: the SpanContext instance
+        :param Any obj: Object to use as context
+        :param dict kwargs: additional data
+        """
+
+    @classmethod
+    def log_kv(cls, span, obj, event, **kwargs):
+        """ Trigger to execute just before closing the span
+
+        :param opentracing.span.Span span: the SpanContext instance
+        :param Any obj: Object to use as context
+        :param str event: Event name
+        :param dict kwargs: additional data
         """
         return span.log_kv({"event": event, **kwargs})
 
-
-class OpenTracingSpan(object):
-    """A container to store the current span"""
-
-    def __init__(self, obj, name, tags=None):
-        self.obj = obj
-        self.name = name
-        self.tags = tags
-        self._span = None
-
     def __enter__(self):
-        """Context guard used by the with statement"""
-        self._span = OpenTracingManager.create_span(
-            self.obj, self.name, self.tags
-        )
-        return self._span
+        """Invoked when span is used as a context manager.
+
+        :return: returns the Span itself
+        """
+        print("---------------->>>>>>>>>>", self.__class__.__name__)
+        if self not in OpenTracingManager._proxies:
+            OpenTracingManager._proxies.append(self)
+        self.prerun(self.span, self.obj)
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """Context guard used by the with statement"""
-        OpenTracingManager.finish_span(self._span)
+        if self in OpenTracingManager._proxies:
+            OpenTracingManager._proxies.remove(self)
+        self.postrun(self.span, self.obj)
+        Span.__exit__(self.span, exc_type, exc_val, exc_tb)
 
 
 class OpenTracingManager(object):
     _registry = dict()
-    _spans = list()
+    _proxies = list()
 
     @classmethod
-    def register(cls, clazz, driver):
-        """ Register a driver for the given class
+    def register(cls, proxy_factory, *classes):
+        """ Register a driver for the given class(es)
 
-        :param Any clazz: Class managed by the driver.
-        :param cdumay_opentracing.OpenTracingDriver driver: Driver to register.
+        :param cdumay_opentracing.SpanProxy proxy_factory: codec to register.
+        :param list classes: Classes managed by the factory.
         """
-        if not issubclass(driver, OpenTracingDriver):
+        if not issubclass(proxy_factory, SpanProxy):
             raise RuntimeError(
-                "Only class which implement OpenTracingDriver are allowed"
+                "Only class which implement SpanProxy are allowed"
             )
-        OpenTracingManager._registry[clazz] = driver
-        logger.debug("Registered {} for {}".format(driver, clazz))
+        for clazz in classes:
+            OpenTracingManager._registry[clazz] = proxy_factory
+            logger.debug("Registered {} for {}".format(proxy_factory, clazz))
 
     @classmethod
-    def get_driver(cls, obj):
-        """ Find registered driver for the given object
+    def get_proxy_factory(cls, obj):
+        """ Return the registered proxy factory for the given object
 
-        :param Any obj: object to manipulate
-        :return: registered driver for this class
-        :rtype: cdumay_opentracing.OpenTracingDriver
+        :param Any obj: Object to use
+        :return: Codec which implement SpanProxy
+        :rtype: cdumay_opentracing.SpanProxy
         """
         for clazz, driver in OpenTracingManager._registry.items():
             if isinstance(obj, clazz):
@@ -114,21 +143,11 @@ class OpenTracingManager(object):
                     driver, obj
                 ))
                 return driver
-        logger.warning("No driver found for {}, default will be used".format(
-            obj
-        ))
-        return OpenTracingDriver
 
-    @classmethod
-    def tags(cls, obj):
-        """ Extract tags from `carrier` object using a registered driver.
-
-        :param Any obj: object to manipulate
-        :return: Tags to add on span
-        :rtype: dict
-        """
-        driver = cls.get_driver(obj)
-        return driver.tags(obj) if driver else dict()
+        logger.warning(
+            "No driver found for {}, default will be used".format(obj)
+        )
+        return SpanProxy
 
     @classmethod
     def get_current_span(cls, obj):
@@ -138,67 +157,72 @@ class OpenTracingManager(object):
         :return: The current span
         :rtype: opentracing.span.Span or None
         """
-        if len(OpenTracingManager._spans) > 0:
-            return OpenTracingManager._spans[-1]
+        if len(OpenTracingManager._proxies) > 0:
+            return OpenTracingManager._proxies[-1].span
         else:
-            driver = cls.get_driver(obj)
-            if driver:
+            proxy_factory = cls.get_proxy_factory(obj)
+            if proxy_factory:
                 try:
-                    return driver.extract(obj)
+                    return proxy_factory.extract(obj)
                 except:
                     pass
 
     @classmethod
-    def create_span(cls, obj, name, tags=None):
+    def create_span(cls, obj, name=None, context=None, tags=None):
         """ Create a new span
 
         :param Any obj: Any object
         :param str name: Span name
+        :param opentracing.span.Span context: Non-managed span (extracted)
         :param dict tags: Additional tags
         :return: Span
-        :rtype: opentracing.span.Span
+        :rtype: OpenTracingSpan
         """
-        parent = cls.get_current_span(obj)
-        driver = cls.get_driver(obj)
         span_tags = tags or dict()
-
+        proxy_factory = cls.get_proxy_factory(obj)
         if obj:
-            span_tags.update(driver.tags(obj))
-        span = opentracing.tracer.start_span(
-            operation_name=name, child_of=parent, tags=span_tags
-        )
-        OpenTracingManager._spans.append(span)
-        return span
+            span_tags.update(proxy_factory.extract_tags(obj))
+
+        proxy = proxy_factory(obj=obj, span=opentracing.tracer.start_span(
+            operation_name=name or proxy_factory.name(obj),
+            child_of=context or OpenTracingManager.get_current_span(obj),
+            tags=tags or proxy_factory.extract_tags(obj)
+        ))
+        OpenTracingManager._proxies.append(proxy)
+        return proxy
 
     @classmethod
-    def finish_span(cls, span=None):
-        """ Terminate the given span
+    def finish_span(cls, proxy=None):
+        """ Terminate the given span proxy
 
-        :param opentracing.span.Span span: Span to finish
+        :param opentracing.SpanProxy proxy: span proxy to finish
         """
-        if span:
-            OpenTracingManager._spans.remove(span)
+        if proxy:
+            OpenTracingManager._proxies.remove(proxy)
         else:
-            span = OpenTracingManager._spans.pop()
+            proxy = OpenTracingManager._proxies.pop()
 
-        if span:
-            span.finish()
+        if proxy:
+            proxy.span.finish()
 
     @classmethod
     def finish_all(cls):
-        for span in OpenTracingManager._spans[::-1]:
-            span.finish()
-        OpenTracingManager._spans = list()
+        for proxy in OpenTracingManager._proxies[::-1]:
+            proxy.span.finish()
+        OpenTracingManager._proxies = list()
 
     @classmethod
-    def log_kv(cls, span, obj, event, **kwargs):
+    def log_kv(cls, event, proxy=None, obj=None, timestamp=None, **kwargs):
         """ Adds a log record to the Span.
 
-        :param opentracing.span.Span span: the Span instance to use.
-        :param Any obj: the `carrier` object.
         :param str event: Span event name.
+        :param cdumay_opentracing.SpanProxy proxy: the span proxy instance to use.
+        :param Any obj: Object to use
+        :param float timestamp: A unix timestamp per time.time(); current time if None
         :param dict kwargs: A dict of string keys and values of any type to log
         :return: Returns the Span itself, for call chaining.
         :rtype: opentracing.span.Span
         """
-        return cls.get_driver(obj).log_kv(span, obj, event, **kwargs)
+        span = proxy.span if proxy else cls.get_current_span(obj)
+        if span:
+            return span.log_kv(dict(event=event, **kwargs), timestamp=timestamp)
